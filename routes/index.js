b541b95a5bc2250a4143ce303882582c3df65cd2
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2018-04-27 12:19:11 -0400

    test

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,15 @@ const fetch = require('node-fetch');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const result = fetch('http://theoldreader.com/kittens/600/400/');
-  result
-    .then(catResponse => catResponse.buffer())
-    .then(buf => {
-      res.write(buf);
-      res.end()
-    });
+  // const result = fetch('http://theoldreader.com/kittens/600/400/');
+  // result
+  //   .then(catResponse => catResponse.buffer())
+  //   .then(buf => {
+  //     res.write(buf);
+  //     res.end()
+  //   });
+  res.write('helloworld');
+  res.end();
 });
 
 module.exports = router;
